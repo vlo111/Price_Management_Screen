0e62ea16c8f8ee0ed0ad7e7bb5f32e4ad5baccc3
Ben <btripp@designitcorp.ca>
2017-06-08 12:26:05 -0700

    Add project files.

---
+++ webapp/App_Start/IdentityConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace CenDek
+{
+    public static class IdentityConfig
+    {
+        public static void RegisterIdentities()
+        {
+            // Ensures the default demo user is available to login with
+            UserManager.Seed();
+        }
+    }
+}
---
+++ webapp/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿#region Using
+
+using System.Web.Mvc;
+
+#endregion
+
+namespace CenDek.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        // GET: home/index
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
