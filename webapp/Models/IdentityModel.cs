d6411f27b7c4b7ffe7235a6bea1f200dd79894eb
Ben <btripp@designitcorp.ca>
2017-06-19 00:46:41 -0700

    removed drop/create db initialization

--- webapp/Models/IdentityModel.cs
+++ webapp/Models/IdentityModel.cs
@@ -29,7 +29,7 @@ namespace CenDek.Models
         {
             // Set the database intializer which is run once during application start
             // This seeds the database with admin user credentials and admin role
-            Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
+            //Database.SetInitializer<ApplicationDbContext>(new ApplicationDbInitializer());
         }
 
         public static ApplicationDbContext Create()
