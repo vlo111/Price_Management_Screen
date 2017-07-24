b2f2e14eb018181fd8b4675b09b62dc101740020
Ben <btripp@designitcorp.ca>
2017-07-23 17:17:00 -0700

    new files for orders

---
+++ webapp/Models/OrderViewModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using DataAccess.Models;
+
+namespace CenDek.Models
+{
+	public class NewCustomerOrder
+	{
+		//display
+        public  DataAccess.Models.Customer customer { get; set; }
+
+		public int customerID { get; set; }
+        [DisplayName("Work Order No")]
+		public string workOrderNumber { get; set; }
+        [DisplayName("Invoice No")]
+        public string invoiceNumber { get; set; }
+		public int contactID { get; set; }
+
+        //pricing
+        [DisplayName("Currency")]
+        public int currencyID { get; set; }
+        [DisplayName("Price Order Date")]
+        public DateTime priceOrderDate { get; set; }
+
+    }
+}
