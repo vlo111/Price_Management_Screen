a7242a764caa10a609de45b34820d31e6dd6a3e1
Johnny <jchang@designitcorp.ca>
2017-08-09 16:51:54 -0700

    CenDek populate scripts minus parts and price

---
+++ Database_Scripts/Populate CenDek Tables.sql
@@ -0,0 +1,120 @@
+
+SET IDENTITY_INSERT [dbo].[Category] ON
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (1, N'DekSmart', 0)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (2, N'Long', 1)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (3, N'Parts', 1)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (4, N'Posts', 1)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (5, N'Glass', 1)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (6, N'Welded', 2)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (7, N'Component', 2)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (8, N'3/4 Posts', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (9, N'6" Shorter', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (10, N'3/4" Extended', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (11, N'3/4" Facia', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (12, N'3/4" 2" Facia', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (13, N'3/4" 1/4" Facia', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (14, N'2 1/2" Posts', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (15, N'2 1/2" Extended Post', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (16, N'2 1/2" 2" Facia', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (17, N'2 1/2" 1/4" Facia', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (18, N'2 1/2" Stop & Go', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (19, N'3/4" Pony Wall Posts', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (20, N'3" Stop & Go', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (21, N'4" Stop & Go', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (22, N'Stop & Go 2" Facia', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (23, N'Pickets', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (24, N'Topless Flat Post Caps', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (25, N'Topless Glass Posts 42 5/8', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (26, N'Topless Glass Posts 38', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (27, N'Topless Glass Posts Custom Height ', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (28, N'Topless Facia Posts 49 7/8"', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (29, N'Topless Facia Posts 45 1/4"', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (30, N'Topless Facia Posts Custom Height', 4)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (31, N'Top Rail Wall Br', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (32, N'Bott Rail Wall Br
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (33, N'Mount Plates & End Cap
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (34, N'Support Legs
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (35, N'Baseplates
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (36, N'Bottom Sleeves
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (37, N'Sleeves 1-3/4" 
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (38, N'Stationary Stair Bracket
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (39, N'Handrail Mounts
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (40, N'Gates
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (41, N'Sleeves 2-1/2"
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (42, N'Baseplate Cover
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (43, N'Facia Brackets
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (44, N'Pipe Handrail
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (45, N'Post Caps
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (46, N'Screws
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (47, N'Glass Tabs
+', 3)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (48, N'Pipe Handrail', 2)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (49, N'Post Material
+', 2)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (50, N'Pickets', 1)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (51, N'5/8"', 50)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (52, N'1 1/2"', 50)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (53, N'Rail', 1)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (54, N'Top', 53)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (55, N'Bottom', 53)
+INSERT [dbo].[Category] ([CategoryID], [Name], [CategoryParentID]) VALUES (56, N'Ornamental', 53)
+SET IDENTITY_INSERT [dbo].[Category] OFF
+SET IDENTITY_INSERT [dbo].[Currency] ON 
+
+INSERT [dbo].[Currency] ([CurrencyID], [Code], [Name]) VALUES (1, N'CAD', N'Canadian Dollar')
+INSERT [dbo].[Currency] ([CurrencyID], [Code], [Name]) VALUES (2, N'USD', N'US Dollar')
+SET IDENTITY_INSERT [dbo].[Currency] OFF
+SET IDENTITY_INSERT [dbo].[MeasUnit] ON 
+
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (1, N'pkg', N'pkg')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (2, N'ea', N'ea')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (3, N'pc', N'pc')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (4, N'bag', N'bag')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (5, N'set', N'set')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (6, N'ft', N'ft')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (7, N'shim', N'shim')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (8, N'wht', N'wht')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (9, N'pad', N'pad')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (10, N'homeshow', N'homeshow')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (11, N'in', N'in')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (12, N'crate', N'crate')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (13, N'attache', N'attache')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (14, N'display', N'display')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (15, N'box', N'box')
+INSERT [dbo].[MeasUnit] ([MeasUnitID], [ShortDescription], [LongDescription]) VALUES (16, N'straight', N'straight')
+SET IDENTITY_INSERT [dbo].[MeasUnit] OFF
+
+
+SET IDENTITY_INSERT [dbo].[PartStatus] ON 
+
+INSERT [dbo].[PartStatus] ([PartStatusID], [Status]) VALUES (1, N'Active')
+INSERT [dbo].[PartStatus] ([PartStatusID], [Status]) VALUES (2, N'On order')
+INSERT [dbo].[PartStatus] ([PartStatusID], [Status]) VALUES (3, N'Discontinued')
+SET IDENTITY_INSERT [dbo].[PartStatus] OFF
+
+SET IDENTITY_INSERT [dbo].[ProductLine] ON 
+
+INSERT [dbo].[ProductLine] ([ProductLineID], [Name], [Description]) VALUES (1, N'Low Pro', NULL)
+INSERT [dbo].[ProductLine] ([ProductLineID], [Name], [Description]) VALUES (2, N'Designer', NULL)
+INSERT [dbo].[ProductLine] ([ProductLineID], [Name], [Description]) VALUES (3, N'Universal', NULL)
+INSERT [dbo].[ProductLine] ([ProductLineID], [Name], [Description]) VALUES (4, N'Low Pro/Uni', NULL)
+INSERT [dbo].[ProductLine] ([ProductLineID], [Name], [Description]) VALUES (5, N'Des/Uni', NULL)
+INSERT [dbo].[ProductLine] ([ProductLineID], [Name], [Description]) VALUES (6, N'Dekworks', NULL)
+INSERT [dbo].[ProductLine] ([ProductLineID], [Name], [Description]) VALUES (7, N'Century', NULL)
+INSERT [dbo].[ProductLine] ([ProductLineID], [Name], [Description]) VALUES (8, N'Beaver', NULL)
+SET IDENTITY_INSERT [dbo].[ProductLine] OFF
