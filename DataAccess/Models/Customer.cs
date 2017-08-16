020a85f10065dd6a8823ce54d3f4649aaf27dc09
Robb Priestley <robb@digitalwizardry.ca>
2017-08-16 11:55:18 -0700

    Removed faulty validation

--- DataAccess/Models/Customer.cs
+++ DataAccess/Models/Customer.cs
@@ -19,38 +19,28 @@ namespace DataAccess.Models
         public int CustomerID { get; set; }
 
         [Required(ErrorMessage = "Company is required")]
-        [RegularExpression("^((?![<>]).)*$", ErrorMessage = "Angle brackets < and > are not allowed.")]
         public string Company { get; set; }
 
         [DisplayName("Phone")]
-        [RegularExpression("^((?![<>]).)*$", ErrorMessage = "Angle brackets < and > are not allowed.")]
         public string PhoneNo { get; set; }
 
-        [RegularExpression("^((?![<>]).)*$", ErrorMessage = "Angle brackets < and > are not allowed.")]
         public string Fax { get; set; }
 
         [DisplayName("Address 1")]
-        [RegularExpression("^((?![<>]).)*$", ErrorMessage = "Angle brackets < and > are not allowed.")]
         public string Address1 { get; set; }
 
         [DisplayName("Address 2")]
-        [RegularExpression("^((?![<>]).)*$", ErrorMessage = "Angle brackets < and > are not allowed.")]
         public string Address2 { get; set; }
 
-        [RegularExpression("^((?![<>]).)*$", ErrorMessage = "Angle brackets < and > are not allowed.")]
         public string City { get; set; }
 
-        [RegularExpression("^((?![<>]).)*$", ErrorMessage = "Angle brackets < and > are not allowed.")]
         public string Province { get; set; }
 
-        [RegularExpression("^((?![<>]).)*$", ErrorMessage = "Angle brackets < and > are not allowed.")]
         public string Country { get; set; }
 
         [DisplayName("Postal Code")]
-        [RegularExpression("^((?![<>]).)*$", ErrorMessage = "Angle brackets < and > are not allowed.")]
         public string PostalCode { get; set; }
 
-        [RegularExpression("^((?![<>]).)*$", ErrorMessage = "Angle brackets < and > are not allowed.")]
         public string Comments { get; set; }
 
         //[Range(0, 100, ErrorMessage = "Please enter a whole number between 0 and 100.")]
